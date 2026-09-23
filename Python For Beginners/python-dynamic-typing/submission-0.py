00d926cbab85eab680fced084ce03dece24ad083
umerniaz05-bip <158862770+umerniaz05-bip@users.noreply.github.com>
2026-09-22 20:04:39 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 12
+print(type(variable))
+variable = 10.1
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Umer"
+print(type(variable))
+variable = [1,2,3]
+print(type(variable))
